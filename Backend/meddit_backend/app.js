f657a9f91fcfe96a91b5dc37c9ac5ca967f560d4
Alina Sheikh <sheikh.alina@gmail.com>
2024-01-27 12:46:02 -0800

    start express

--- Backend/meddit_backend/app.js
+++ Backend/meddit_backend/app.js
@@ -1,11 +1,16 @@
 import express from "express";
 import { dirname } from 'path';
+import path from 'path';
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + "../Frontend/meddit_frontend/public/index.html");
+    res.sendFile(path.join(__dirname, '../../Frontend/meddit_frontend/public', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
 });
