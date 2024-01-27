d4e1a84bcf13ca255b5824549643cdb29b6e3f8a
Rithwik Garapati <rithwik.garapati@gmail.com>
2024-01-27 13:59:08 -0800

    added login page

--- Frontend/meddit_frontend/src/landing_page/pages/AboutUs.js
+++ Frontend/meddit_frontend/src/landing_page/pages/AboutUs.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 import data from './data.js'
+import './AboutUs.css'
 
+// This is a div for the single block of text.
 const SingleDiv = props => {
     return (
         <div className="info">
@@ -11,15 +13,61 @@ const SingleDiv = props => {
     )
 };
 
+// This is the login side
+const Login  = (props) => {
+    const [email, setEmail] = useState("");
+
+    const onButtonClick = () => {
+        // You'll update this function later...
+    }
+
+
+    return(
+    <div className={"mainContainer"}>
+        <div className={"titleContainer"}>
+            <div>Login</div>
+        </div>
+        <br />
+        <div className={"inputContainer"}>
+            <input
+                value={email}
+                placeholder="Enter your email here"
+                onChange={ev => setEmail(ev.target.value)}
+                className={"inputBox"} />
+        </div>
+        <br />
+        <div className={"inputContainer"}>
+            <input
+                className={"inputButton"}
+                type="button"
+                onClick={onButtonClick}
+                value={"Log in"} />
+        </div>
+    </div>
+
+    )
+}
+
+// Having all our divs on the landing page.
 const AboutUs = () => {
     const AllDivs = data.map(info => {
-        return <SingleDiv 
-            title={info.title}
-            content={info.content}
-        />
-    })
+        return (
+            <SingleDiv 
+                title={info.title}
+                content={info.content}
+            />
+        )  
 
-    return AllDivs;
+    })
+    return(
+        <div className="main">
+            <div>
+                {AllDivs}
+            </div>
+            
+            <Login />
+        </div>
+    ) 
 };
 
 export default AboutUs;
--- Frontend/meddit_frontend/src/landing_page/pages/data.js
+++ Frontend/meddit_frontend/src/landing_page/pages/data.js
@@ -3,12 +3,12 @@ import React from "react";
 const data = [
     {
         title: 'Problem',
-        content: 'NO'
+        content: 'Medical Errors, including equipment failure, improper transfusions, inaccurate medication recommendations, and misdiagnoses, are the cause of the deaths of over 250,000 Americans annually, making it the third leading cause of death in the U.S. behind heart disease and cancer. How do we combat this?'
     },
 
     {
         title: 'Solution',
-        content: 'YES'
+        content: 'Introducing Meddit: a collaborative platform designed exclusively for every hospital\'s medical community! By fostering a discussion forum for each hospital\'s medical workers, Meddit creates a comfortable, safe, and dynamic space for medical workers to anonymously share insights, seek advice, and confirm their conclusions without the fear of judgement or decidophobia.'
     }
 ]
 
