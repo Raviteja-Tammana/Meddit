c761f7d01e4923e4c87b75125973ee521af13345
Rithwik Garapati <rithwik.garapati@gmail.com>
2024-01-27 17:47:44 -0800

    bare bones of feed

--- Frontend/meddit_frontend/src/feed/feed.js
+++ Frontend/meddit_frontend/src/feed/feed.js
@@ -1,9 +1,26 @@
+import React from 'react'
+import Post from './Post';
 
+import DUMMY_DATA from './data';
+import './feed.css'
 
 const Feed = () => {
+    const AllInfo = DUMMY_DATA.map(info => {
+        return(
+            <Post 
+                title={info.title}
+                date = {info.date}
+                content = {info.content}
+            />
+        )
+    })
     return(
-        <h1>Feed</h1>
+       <div className='allposts'>
+            {AllInfo}
+       </div>
     )
 }
+
+
 
 export default Feed;
