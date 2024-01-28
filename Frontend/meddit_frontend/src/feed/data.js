818ee011feab487f2f2a2966e500b4b6a3907cb3
Raviteja Tammana <tammanaravit25@gmail.com>
2024-01-27 18:57:24 -0800

    finalized example posts

--- Frontend/meddit_frontend/src/feed/data.js
+++ Frontend/meddit_frontend/src/feed/data.js
@@ -2,28 +2,28 @@
 
 const DUMMY_DATA = [
     {
-        postID: 'kothi baby', 
+        postID: '419273', 
         title: 'Misdiagnosis of Thyroid Disorder', 
         user: 'monkeyman', 
         date: 'January 27, 2024', 
-        likes: '78', 
+        likes: '79', 
         content: 'I wanted to share an experience I had with a Misdiagnosis Error. A patient of mine came to me with symptoms of fatigue, weight loss, and increased thirst. Based on these symptoms, I diagnosed the patient with diabetes mellitus. However, it was later discovered that the patient\'s symptoms were actually due to an underlying thyroid disorder. My misdiagnosis led to the delayed treatment of the patient\'s actual condition, impacting the patient\'s health. If anyone has any advice on what else I should have looked for in my diagnosis, I would greatly appreciate a response.'
     },
     {
-        postID: 'kothi baby', 
-        title: 'Misdiagnosis of Thyroid Disorder', 
-        user: 'monkeyman', 
+        postID: '103958', 
+        title: 'Pulse Oximeter Equipment Failure', 
+        user: 'donkeygirl', 
         date: 'January 26, 2024', 
         likes: '68', 
-        content: 'I wanted to share an experience I had with a Misdiagnosis Error. A patient of mine came to me with symptoms of fatigue, weight loss, and increased thirst. Based on these symptoms, I diagnosed the patient with diabetes mellitus. However, it was later discovered that the patient\'s symptoms were actually due to an underlying thyroid disorder. My misdiagnosis led to the delayed treatment of the patient\'s actual condition, impacting the patient\'s health. If anyone has any advice on what else I should have looked for in my diagnosis, I would greatly appreciate a response.'
+        content: 'While I was monitoring one of my patients, I used the pulse oximeter to find their oxygen saturation levels. The device displayed a consistent reading within the normal range, giving no indication of any problem. However, after some time, my patient came back with feelings of respiratory distress, and after self-analysis I realized that the pulse oximeter experienced a malfunction during our appointment, display invalid readings. My patient underwent a lot of distress due to my mistake, and I would love any feedback on how I could have been more careful in that situation and ensured that the pulse oximeter was working properly before looking at the readings.'
     },
     {
-        postID: 'kothi baby', 
-        title: 'Misdiagnosis of Thyroid Disorder', 
-        user: 'monkeyman', 
+        postID: '059381', 
+        title: 'Medication Communition Mishap', 
+        user: 'chonkeyman', 
         date: 'January 25, 2024', 
         likes: '267', 
-        content: 'I wanted to share an experience I had with a Misdiagnosis Error. A patient of mine came to me with symptoms of fatigue, weight loss, and increased thirst. Based on these symptoms, I diagnosed the patient with diabetes mellitus. However, it was later discovered that the patient\'s symptoms were actually due to an underlying thyroid disorder. My misdiagnosis led to the delayed treatment of the patient\'s actual condition, impacting the patient\'s health. If anyone has any advice on what else I should have looked for in my diagnosis, I would greatly appreciate a response.'
+        content: 'I quickly wanted to bring light to an incident I just had in hopes that this post\'s audience would learn from my mistake. A patient came to me asking for a medication that was prescribed to them by their doctor. However, as I was in a hectic environment, I misheard the name of the medication and instead gave my patient a similar-sounding but incorrect medication. The patient came back in a couple of days, complaining about some complications she experienced from the medication, and after further review, it was realised that the patient received the wrong medication. I made a big mistake, and my patient suffered because of it. To all of my readers, make sure to always pay your full attention in every situation with a patient to prevent situations like this from happening.'
     }
 ]
 
