277f96f4089ccd649e1b8a23ecc4d344f57e1427
Rithwik Garapati <rithwik.garapati@gmail.com>
2024-01-28 11:55:42 -0800

    completed frontend

--- Frontend/meddit_frontend/src/feed/IndividualPost.js
+++ Frontend/meddit_frontend/src/feed/IndividualPost.js
@@ -1,13 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import './IndividualPost.css'
 import { useParams } from "react-router-dom";
+import Post from "./Post";
+
+
 
 const IndividualPost = () => {
     let { id } = useParams();
+
+    fetch('http://localhost:8080/expanded', {
+        method: 'POST',
+        body: JSON.stringify(
+            {
+                postID: id,
+            }),
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+            },
+    }).then(function(response) {
+        return response.json();
+    }).then(function(data){
+
+        <Post 
+            title={data[0].title}
+            date = {data[0].date}
+            content = {data[0].content}
+            id={data[0].postID}
+            likes={data[0].likes}
+        />
+    })
+
+    const [solutions, setSolutions ] = useState("");
+    const [experiences, setExpereiences] = useState("");
+    
     return(
-        <h1>
-            This is the user id: {id}
-        </h1>
+
+        <div className="sections">
+            <textarea
+                placeholder="Propose Solution"
+                onChange={ev => setSolutions(ev.target.value)}
+                className={"postsol" }
+                >
+            </textarea>
+
+            <textarea
+                placeholder="Similar Experience"
+                onChange={ev => setExpereiences(ev.target.value)}
+                className={"similarexp"}
+                >
+            </textarea>
+
+        </div>
     )
 }
 
